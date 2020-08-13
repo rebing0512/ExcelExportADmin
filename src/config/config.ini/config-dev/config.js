4a04568758de24f7cfbda851966c59814ed61ce5
wangjian <wangjian@mbcore.com>
2020-08-13 11:13:53 +0800

    debug

--- src/config/config.ini/config-dev/config.js
+++ src/config/config.ini/config-dev/config.js
@@ -1,49 +1,41 @@
 /**
- * Created by fengquanju on 2019/10/5.
+ * Created by Jenson on 2020/08/12.
  */
-
 
 var domainConfig = {
     pmCore:'https://mbdev-pettyb.pettyb.com/',  //【*】
     pmStorage:'https://mbdev-pettyb-storage.pettyb.com/',
-    groupName:'dev_cpa_pca_',
-    baseUrl:'https://mbdev-wohand-partner-api.pettyb.com/'
+    groupName:'dev_pz_pca_',
+    baseUrlClient:'https://mbdev-purezen.pettyb.com/',
+    baseUrl:'https://mbdev-purezen-api.pettyb.com/'
 };
-
-
-
 
 //验证机制
 var Config = {
-
     /*Var*/
     'VLSAccessToken':domainConfig.groupName+'access_token',  /* Var localStorage*/
     'VLSAuthToken':domainConfig.groupName+'auth_token',
-    'VLSClient':domainConfig.groupName+'mbcore_client',  /* Var localStorage*/  //【*】
-
+    'VLSClient':domainConfig.groupName+'mbcore_client',  /* Var localStorage*/
     'Mode':'development',/*默认是开发模式，如果是prodect即是产品模式*/
-
     /*Api*/
     'GrantTypeAccessToken':'client_credential',  //grant_type
-    'AppAppid':'TfZ4wruPFDWZTEwPwqutxW1c7XsHK9H',  //appid
-    'AppSecret':'6B7HQ2XSK4RLGHtRrFJNcR2dRIIDNrZa', //secret
-    'AppChannel':'pcadmin',  //channel
-    'AppGroupId':'14' //group_id
-
-
+    'AppAppid':'oZokhz16Eh2ckYFIghxGsBPdkAuoSnSp',  //appid oZokhz16Eh2ckYFIghxGsBPdkAuoSnSp
+    'AppSecret':'1THIf7WQhlyTPpjxdYRnwbWDx0TkaPPO', //secret 1THIf7WQhlyTPpjxdYRnwbWDx0TkaPPO
+    'AppChannel':'pcadmin',  // channel
+    'AppGroupId':'8' //group_id
 };
 
 var AppUrl = {
-    Index:domainConfig.baseUrl + 'CityPartnersAdmin/index.html',
-    Login:domainConfig.baseUrl+'CityPartnersAdmin/login.html',
-}
+    Index:domainConfig.baseUrlClient + 'PartnerAdmin/index.html',
+    Login:domainConfig.baseUrlClient+'PartnerAdmin/login.html',
+};
 
-//用户中心接口
+// Token、图片以及登录接口
 var ApiUrl = {
-    //   // 新接口
-    'getAccessToken': domainConfig.pmCore +'core/oauth/getAccessToken',   //--------【 * ok】
-    'refreshAccessToken': domainConfig.pmCore +'core/oauth/refreshAccessToken',  //--------【ok】
+    // 接口
+    'getAccessToken': domainConfig.pmCore +'core/oauth/getAccessToken',
+    'refreshAccessToken': domainConfig.pmCore +'core/oauth/refreshAccessToken',
     'getImageUri':domainConfig.pmStorage +'getImageUri',
     'getUploadImageToken':domainConfig.pmStorage +'getUploadImageToken',
-    LoginApi:domainConfig.baseUrl + 'admin/login',//登录接口
+    LoginApi:domainConfig.baseUrl + 'admin/partner/login', // 登录接口
 };
